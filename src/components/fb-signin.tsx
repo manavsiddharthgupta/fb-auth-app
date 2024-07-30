56450295eb8e07a28666acba0c8831ae6242a8d7
Manav Gupta <2006468@kiit.ac.in>
2024-07-31 02:42:49 +0530

    refactor: Update Facebook sign-in button to use new SVG icon

--- src/components/fb-signin.tsx
+++ src/components/fb-signin.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import google from '@/svgs/google.svg'
+import fb from '@/svgs/fb.svg'
 import { useFacebook } from './fb-session'
 
 const FacebookSignInBtn = () => {
   const { connectApp } = useFacebook()
   return (
     <Button onClick={() => connectApp()} variant='default' className='mt-4'>
-      <Image src={google} alt='google' width={24} height={24} />
+      <Image src={fb} alt='fb' width={24} height={24} />
       <span className='ml-2'>Continue With Facebook</span>
     </Button>
   )
